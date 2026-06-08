7676dd634d38bf38c98da95d47982cf8849fa680
Bhaskoro V <bhaskorou@gmail.com>
2026-06-08 18:10:50 +0700

    loop for go

--- tipedata.go
+++ tipedata.go
@@ -7,9 +7,13 @@ func tipedata() {
 	var nama string = "bhaskoro prayoga"
 	var umur int = 22
 	var phi float32 = 3.14
+	var i int;
 
-	fmt.Println("hallo, nama saya ", nama);
-	fmt.Println("umur saya ", umur);
+	fmt.Println("hallo, nama saya", nama);
+	fmt.Println("umur saya", umur);
 	fmt.Println("bilangan desimal", phi);
 
+	for i=0; i<=10; i++ {
+		fmt.Printf("ini pengulangan ke %d\n", i);
+	}
 }
