b0cf793a3464dbafd55e1f0dd73ec8375f937208
Bhaskoro <bhaskorou@gmail.com>
2026-06-09 11:52:21 +0700

    switch case go

--- main.go
+++ main.go
@@ -1,10 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
 
-func main(){
-	tipedata()
-	loop()
-	dowhile()
-	Tuser()
+func main() {
 
+	var pilihan int
+
+	for {
+		fmt.Println("1. tipedata")
+		fmt.Println("2. loop")
+		fmt.Println("3. do while")
+		fmt.Println("4. tuser")
+		fmt.Println("0. exit")
+		fmt.Println("masukan pilihan")
+		fmt.Scanln(&pilihan)
+
+		switch pilihan {
+		case 1:
+			fmt.Println("tipe data")
+			tipedata()
+		case 2:
+			fmt.Println("loop")
+			loop()
+		case 3:
+			fmt.Println("do while")
+			dowhile()
+		case 4:
+			fmt.Println("tuser")
+			Tuser()
+		case 0:
+			fmt.Println("terimakasih")
+			os.Exit(0)
+		default:
+			fmt.Println("tidak valid")
+		}
+	}
 }
--- tipedata.go
+++ tipedata.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func tipedata() {
 
@@ -47,7 +50,7 @@ func Tuser() {
 		fmt.Scan(&pilih)
 
 		if pilih != "y"{
-			break
+			os.Exit(0)
 		}
 	}
 }
